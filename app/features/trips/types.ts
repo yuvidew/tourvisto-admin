afbd3b068191db7f11511201a7aad4dd64ac3a57
Yuvi dew <yuvraj@truelink.ai>
2025-07-06 11:21:38 +0530

    feat : creating a types file to add the type and also import

---
+++ app/features/trips/types.ts
@@ -0,0 +1,11 @@
+export type TripsType = {
+    userId: number,
+    country: string,
+    duration: string,
+    group_type: string,
+    travel_style: string,
+    interests: string,
+    budget_estimate: string
+    images: string,
+    result: string,
+}
